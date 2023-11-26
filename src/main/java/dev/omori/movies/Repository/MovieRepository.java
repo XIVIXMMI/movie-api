8382e318289a90343a666fe3f8582b7484c18ecf
XIVIXMMI <lethaikhoinguyen@gmail.com>
2023-11-26 22:22:51 +0700

    Fist commit

---
+++ src/main/java/dev/omori/movies/Repository/MovieRepository.java
@@ -0,0 +1,11 @@
+package dev.omori.movies.Repository;
+
+import dev.omori.movies.models.Movie;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+
+}
