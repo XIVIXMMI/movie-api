f16810dbdca2466e82064b935f5af19ab7dc445f
XIVIXMMI <lethaikhoinguyen@gmail.com>
2023-11-27 18:02:10 +0700

    create method POST for review

--- src/main/java/dev/omori/movies/Repository/ReviewRepository.java
+++ src/main/java/dev/omori/movies/Repository/ReviewRepository.java
@@ -1,13 +1,10 @@
 package dev.omori.movies.Repository;
 
-import dev.omori.movies.models.Movie;
+import dev.omori.movies.models.Review;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
-    Optional<Movie> findMovieByImdbId(String imdbId);
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
 }
--- src/main/java/dev/omori/movies/Service/MovieService.java
+++ src/main/java/dev/omori/movies/Service/MovieService.java
@@ -2,10 +2,12 @@ package dev.omori.movies.Service;
 
 import dev.omori.movies.Repository.MovieRepository;
 import dev.omori.movies.models.Movie;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -13,5 +15,8 @@ public class MovieService {
     private MovieRepository movieRepository;
     public List<Movie> allMovies(){
         return movieRepository.findAll();
+    }
+    public Optional<Movie> singleMovie(String imdbId){
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 }
